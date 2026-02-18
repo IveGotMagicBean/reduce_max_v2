718c5fef911c8d2bad0a9161432a1dc218b476f7
IveGotMagicBean <542058929@qq.com>
2026-02-18 21:53:09 +0800

    Add multi-core optimization: SetBlockDim(20)

---
+++ build_out/op_kernel/CMakeFiles/ReduceMaxCustom_ascend910b.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for ReduceMaxCustom_ascend910b.
+# This may be replaced when dependencies are built.
---
+++ build_out/op_kernel/CMakeFiles/ascendc_bin_ascend910b_gen_ops_config.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ascendc_bin_ascend910b_gen_ops_config.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
